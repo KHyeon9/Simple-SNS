01330ff701c89e9627d8508a537d50751d456c87
git에서 Khyeon9 <git mrk19967@gmail.com>
2024-06-22 20:20:38 +0900

    User 필드와 필요한 enum, response 작성

--- src/main/java/com/simple/sns/entity/UserEntity.java
+++ src/main/java/com/simple/sns/entity/UserEntity.java
@@ -1,19 +1,25 @@
 package com.simple.sns.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import com.simple.sns.model.UserRole;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
+import java.sql.Timestamp;
+import java.time.Instant;
 
 @Getter
 @Setter
-@Table
+@Table(name = "\"user\"")
+@SQLDelete(sql = "UPDATE \"user\" SET deleted_at = NOW() where id=?")
+@Where(clause = "deleted_at is NULL")
 @Entity
 public class UserEntity {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     @Column(name = "user_name")
@@ -21,4 +27,35 @@ public class UserEntity {
 
     @Column(name = "password")
     private String password;
+
+    @Column(name = "role")
+    @Enumerated(EnumType.STRING)
+    private UserRole role = UserRole.USER;
+
+    @Column(name = "registered_at")
+    private Timestamp registeredAt;
+
+    @Column(name = "updated_at")
+    private Timestamp updatedAt;
+
+    @Column(name = "deleted_at")
+    private Timestamp deletedAt;
+
+    @PrePersist
+    void registeredAt() {
+        this.registeredAt = Timestamp.from(Instant.now());
+    }
+
+    @PreUpdate
+    void updatedAt() {
+        this.updatedAt = Timestamp.from(Instant.now());
+    }
+
+    public static UserEntity of(String userName, String password) {
+        UserEntity userEntity = new UserEntity();
+        userEntity.setUserName(userName);
+        userEntity.setPassword(password);
+
+        return userEntity;
+    }
 }
--- src/main/java/com/simple/sns/model/User.java
+++ src/main/java/com/simple/sns/model/User.java
@@ -1,8 +1,34 @@
 package com.simple.sns.model;
 
-// TODO: implement
+import com.simple.sns.entity.UserEntity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.sql.Timestamp;
+
+@Getter
+@AllArgsConstructor
 public class User {
 
+    private Integer id;
     private String userName;
     private String password;
+    private UserRole userRole;
+    private Timestamp registeredAt;
+    private Timestamp updatedAt;
+    private Timestamp deletedAt;
+
+
+    // entity를 dto로 변경
+    public static User from(UserEntity entity) {
+        return new User(
+                entity.getId(),
+                entity.getUserName(),
+                entity.getPassword(),
+                entity.getRole(),
+                entity.getRegisteredAt(),
+                entity.getUpdatedAt(),
+                entity.getDeletedAt()
+        );
+    }
 }
