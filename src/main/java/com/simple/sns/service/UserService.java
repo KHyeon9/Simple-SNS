4bd1a1852a58efa367d94ab3a68ff88db555e847
git에서 Khyeon9 <git mrk19967@gmail.com>
2024-06-22 20:25:12 +0900

    회원 가입 부분 간단하게 구현
    오류 등을 고려한 추가적 구현 필요함

--- src/main/java/com/simple/sns/service/UserService.java
+++ src/main/java/com/simple/sns/service/UserService.java
@@ -1,13 +1,12 @@
 package com.simple.sns.service;
 
 import com.simple.sns.entity.UserEntity;
+import com.simple.sns.exception.ErrorCode;
 import com.simple.sns.exception.SnsApplicationException;
 import com.simple.sns.model.User;
 import com.simple.sns.repository.UserEntityRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
-import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -18,22 +17,24 @@ public class UserService {
     // TODO: implement
     public User join(String userName, String password) {
         // 회원 가입하는 username 중복 체크
-        Optional<UserEntity> userEntity = userEntityRepository.findByUserName(userName);
+        userEntityRepository.findByUserName(userName).ifPresent(it -> {
+                throw new SnsApplicationException(ErrorCode.DUPLICATED_USER_NAME, String.format("%s is duplicated", userName));
+        });
 
         // 회원 가입 진행 -> user 등록
-        userEntityRepository.save(new UserEntity());
+        UserEntity userEntity = userEntityRepository.save(UserEntity.of(userName, password));
 
-        return new User();
+        return User.from(userEntity);
     }
 
     // TODO: implement
     public String login(String userName, String password) {
         // 회원가입 체크
-        UserEntity userEntity = userEntityRepository.findByUserName(userName).orElseThrow(SnsApplicationException::new);
+        UserEntity userEntity = userEntityRepository.findByUserName(userName).orElseThrow(() -> new SnsApplicationException(ErrorCode.DUPLICATED_USER_NAME, ""));
 
         // 비밀번호 체크
         if (!userEntity.getPassword().equals(password)) {
-            throw new SnsApplicationException();
+            throw new SnsApplicationException(ErrorCode.DUPLICATED_USER_NAME, "");
         }
         
         // 토큰 생성
