2cd5dbc49c28668502a442f9184d5c2ad9e946ad
git에서 Khyeon9 <git mrk19967@gmail.com>
2024-06-23 19:04:04 +0900

    Post관련 엔티티, 리포지토리 생성

---
+++ src/main/java/com/simple/sns/entity/PostEntity.java
@@ -0,0 +1,52 @@
+package com.simple.sns.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
+import java.sql.Timestamp;
+import java.time.Instant;
+
+@Getter
+@Setter
+@Table(name = "\"post\"")
+@SQLDelete(sql = "UPDATE \"user\" SET deleted_at = NOW() where id=?")
+@Where(clause = "deleted_at is NULL")
+@Entity
+public class PostEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "content", columnDefinition = "TEXT")
+    private String content;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private UserEntity user;
+
+    @Column(name = "registered_at")
+    private Timestamp registeredAt;
+
+    @Column(name = "updated_at")
+    private Timestamp updatedAt;
+
+    @Column(name = "deleted_at")
+    private Timestamp deletedAt;
+
+    @PrePersist
+    void registeredAt() {
+        this.registeredAt = Timestamp.from(Instant.now());
+    }
+
+    @PreUpdate
+    void updatedAt() {
+        this.updatedAt = Timestamp.from(Instant.now());
+    }
+}
---
+++ src/main/java/com/simple/sns/repository/PostEntityRepository.java
@@ -0,0 +1,9 @@
+package com.simple.sns.repository;
+
+import com.simple.sns.entity.PostEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostEntityRepository extends JpaRepository<PostEntity, Integer> {
+}
