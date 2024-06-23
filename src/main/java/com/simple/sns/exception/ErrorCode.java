91ea6932e377f93dc699580a87be782b7b6f03b7
git에서 Khyeon9 <git mrk19967@gmail.com>
2024-06-23 17:21:19 +0900

    에러 발생할 때, 해당 에러에 맞는 케이스 추가

--- src/main/java/com/simple/sns/exception/ErrorCode.java
+++ src/main/java/com/simple/sns/exception/ErrorCode.java
@@ -8,7 +8,11 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum ErrorCode {
 
-    DUPLICATED_USER_NAME(HttpStatus.CONFLICT, "User name is duplicated"),;
+    DUPLICATED_USER_NAME(HttpStatus.CONFLICT, "User name is duplicated"),
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "Internal server error"),
+    INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, "Password is invalid"),
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "User not founded"),
+    ;
 
     private HttpStatus status;
     private String message;
