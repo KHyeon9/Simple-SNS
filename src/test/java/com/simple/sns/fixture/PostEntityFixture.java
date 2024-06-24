b441c7f105d7e35acee2250d23296ed26f1bf4b1
git에서 Khyeon9 <git mrk19967@gmail.com>
2024-06-24 19:49:34 +0900

    fixture에 userId를 받는 매개 변수 추가

--- src/test/java/com/simple/sns/fixture/PostEntityFixture.java
+++ src/test/java/com/simple/sns/fixture/PostEntityFixture.java
@@ -5,9 +5,9 @@ import com.simple.sns.entity.UserEntity;
 
 public class PostEntityFixture {
 
-    public static PostEntity get(String userName, Integer postId) {
+    public static PostEntity get(String userName, Integer postId, Integer userId) {
         UserEntity userEntity = new UserEntity();
-        userEntity.setId(1);
+        userEntity.setId(userId);
         userEntity.setUserName(userName);
 
         PostEntity postEntity = new PostEntity();
