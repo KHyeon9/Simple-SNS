2e3aa5aca2485bb96ea121fd6c0db07892fef379
git에서 Khyeon9 <git mrk19967@gmail.com>
2024-06-24 19:06:24 +0900

    에러 코드에 필요한 에러 코드 추가
    * INVALID_PERMISSION
    * POST_NOT_FOUND
    * INTERNAL_SERVER_ERROR
    위 3 종류 추가

--- src/main/java/com/simple/sns/exception/ErrorCode.java
+++ src/main/java/com/simple/sns/exception/ErrorCode.java
@@ -9,10 +9,12 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
 
     DUPLICATED_USER_NAME(HttpStatus.CONFLICT, "User name is duplicated"),
-    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "Internal server error"),
     INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, "Password is invalid"),
     INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "token is invalid"),
+    INVALID_PERMISSION(HttpStatus.UNAUTHORIZED, "Permission is invalid"),
+    POST_NOT_FOUND(HttpStatus.NOT_FOUND, "Post not founded"),
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "User not founded"),
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "Internal server error"),
     ;
 
     private HttpStatus status;
