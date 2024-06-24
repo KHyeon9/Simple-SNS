b3863ceafe657b413d02439619efce1c0410566c
git에서 Khyeon9 <git mrk19967@gmail.com>
2024-06-24 19:48:59 +0900

    PostController에 modify 작성

--- src/main/java/com/simple/sns/controller/PostController.java
+++ src/main/java/com/simple/sns/controller/PostController.java
@@ -1,14 +1,14 @@
 package com.simple.sns.controller;
 
 import com.simple.sns.controller.request.PostCreateRequest;
+import com.simple.sns.controller.request.PostModifyRequest;
+import com.simple.sns.controller.response.PostResponse;
 import com.simple.sns.controller.response.Response;
+import com.simple.sns.model.Post;
 import com.simple.sns.service.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/posts")
@@ -22,5 +22,12 @@ public class PostController {
         postService.create(request.getTitle(), request.getContent(), authentication.getName());
 
         return Response.success();
+    }
+
+    @PutMapping("/{postId}")
+    public Response<PostResponse> modify(@PathVariable Integer postId, @RequestBody PostModifyRequest request, Authentication authentication) {
+        Post post = postService.modify(request.getTitle(), request.getContent(), authentication.getName(), postId);
+
+        return Response.success(PostResponse.fromPost(post));
     }
 }
