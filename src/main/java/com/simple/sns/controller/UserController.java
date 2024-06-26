eda7b7c1f8fe7ec4cb4d295ef66ad700dda361a5
git에서 Khyeon9 <git mrk19967@gmail.com>
2024-06-26 21:27:21 +0900

    @Slf4j 주석 추가

--- src/main/java/com/simple/sns/controller/UserController.java
+++ src/main/java/com/simple/sns/controller/UserController.java
@@ -8,14 +8,16 @@ import com.simple.sns.controller.response.UserLoginResponse;
 import com.simple.sns.model.User;
 import com.simple.sns.service.UserService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequiredArgsConstructor
-@RequestMapping("/api/v1/users")
+@Slf4j
 @RestController
+@RequestMapping("/api/v1/users")
+@RequiredArgsConstructor
 public class UserController {
 
     private final UserService userService;
